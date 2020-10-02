f1a9e0fc0ec052dbca779e96642de43f8389d9bb
JAPSIMRAN SINGH <65735725+japsimrans13@users.noreply.github.com>
2020-10-02 13:31:55 +0530

    Added While loop to the program
    Added While loop to the program to make this program check infinite the prime no. infinite times.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-n = int(input('Enter no. here:'))
-prime='t'  
-for i in range (2,n):
-    if n% i == 0:
-        prime = 'fail'
-        break
-  
-if prime=='fail':
-    print(n,'is not prime')
-else:
-    print(n,'is prime')
+while True:
+    n = int(input('Enter no. here:'))
+    prime='t'  
+    for i in range (2,n):
+        if n% i == 0:
+            prime = 'fail'
+            break
+    
+    if prime=='fail':
+        print(n,'is not prime')
+    else:
+        print(n,'is prime')
